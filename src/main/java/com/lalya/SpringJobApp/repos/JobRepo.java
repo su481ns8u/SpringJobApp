6e52e1177f110456df8aaafa87f262f3cf9ba2e9
Sudhanshu Ghinmine <sudhanshu.ghinmine@outlook.com>
2025-02-14 02:36:24 +0530

    Updated app to use data JPA and postgresql

--- src/main/java/com/lalya/SpringJobApp/repos/JobRepo.java
+++ src/main/java/com/lalya/SpringJobApp/repos/JobRepo.java
@@ -1,6 +1,7 @@
 package com.lalya.SpringJobApp.repos;
 
 import com.lalya.SpringJobApp.model.JobPost;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,41 +10,8 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class JobRepo {
-    private List<JobPost> jobsList = new ArrayList<>(Arrays.asList(
-            new JobPost(1, "Full Stack Java", 3, "Java Dev", Arrays.asList("Java", "Spring")),
-            new JobPost(2, "Full Stack Web", 5, "React Dev", Arrays.asList("JS", "React")),
-            new JobPost(3, "Full Stack iOS", 6, "Swift Dev", Arrays.asList("Swift"))
-    ));
+public interface JobRepo extends JpaRepository<JobPost, Integer> {
 
-    public void addJob(JobPost jobPost) {
-        this.jobsList.add(jobPost);
-    }
+    List<JobPost> findByPostProfileContainingOrPostDescContaining(String postProfile, String postDesc);
 
-    public List<JobPost> getAllJobs() {
-        return jobsList;
-    }
-
-    public JobPost getJob(int jobId) {
-        return getJobById(jobId);
-    }
-
-    public void updateJob(JobPost updateJobPost) {
-        JobPost updatedJobPost = getJobById(updateJobPost.getPostId());
-        updatedJobPost.setPostDesc(updateJobPost.getPostDesc());
-        updatedJobPost.setPostProfile(updateJobPost.getPostProfile());
-        updatedJobPost.setPostTechStack(updateJobPost.getPostTechStack());
-        updatedJobPost.setReqExperience(updateJobPost.getReqExperience());
-    }
-
-    public void deleteJob(int jobId) {
-        jobsList.remove(getJobById(jobId));
-    }
-
-    public JobPost getJobById(int jobId) {
-        return jobsList.stream()
-                .filter(jobPost -> jobPost.getPostId() == jobId)
-                .findFirst()
-                .orElse(new JobPost(0, "No job", 0, "na", new ArrayList<>()));
-    }
 }
--- src/main/java/com/lalya/SpringJobApp/service/JobService.java
+++ src/main/java/com/lalya/SpringJobApp/service/JobService.java
@@ -5,29 +5,45 @@ import com.lalya.SpringJobApp.repos.JobRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class JobService {
+
+    private List<JobPost> jobsList = new ArrayList<>(Arrays.asList(
+            new JobPost(1, "Full Stack Java", 3, "Java Dev", Arrays.asList("Java", "Spring")),
+            new JobPost(2, "Full Stack Web", 5, "React Dev", Arrays.asList("JS", "React")),
+            new JobPost(3, "Full Stack iOS", 6, "Swift Dev", Arrays.asList("Swift"))
+    ));
     @Autowired
     JobRepo jobRepo;
     public void addJob(JobPost jobPost) {
-        jobRepo.addJob(jobPost);
+        jobRepo.save(jobPost);
     }
 
     public List<JobPost> getAllJobs() {
-        return jobRepo.getAllJobs();
+        return jobRepo.findAll();
     }
 
     public JobPost getJob(int jobId) {
-        return jobRepo.getJob(jobId);
+        return jobRepo.findById(jobId).orElse(new JobPost(0, "No job", 0, "na", new ArrayList<>()));
     }
 
     public void updateJob(JobPost jobPost) {
-        jobRepo.updateJob(jobPost);
+        jobRepo.save(jobPost);
     }
 
     public void deleteJob(int jobId) {
-        jobRepo.deleteJob(jobId);
+        jobRepo.deleteById(jobId);
+    }
+
+    public void load() {
+        jobRepo.saveAll(jobsList);
+    }
+
+    public List<JobPost> search(String keyword) {
+        return jobRepo.findByPostProfileContainingOrPostDescContaining(keyword, keyword);
     }
 }
